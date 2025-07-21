abe720500934a4f73fddd529ba2392088a7a61e0
krunal171 <ranakrunal176@gmail.com>
2025-07-21 23:45:14 +0530

    Add files via upload

---
+++ Q11/index.js
@@ -0,0 +1,2 @@
+const { greet } = require('krunal-q11-utils');
+console.log(greet("Krunal")); // Hello, Krunal! Welcome to my package.
---
+++ krunal-q11-utils/index.js
@@ -0,0 +1,6 @@
+// index.js
+function greet(name) {
+  return `Hello, ${name}! Welcome to my package.`;
+}
+
+module.exports = { greet };
